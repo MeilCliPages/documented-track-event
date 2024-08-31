5bd86f2dbecae997928ce273d6d7ca5e7e830cee
MeilCli <ics.sora.meil@gmail.com>
2024-08-31 15:03:14 +0900

    add basic components

---
+++ src/constant.ts
@@ -0,0 +1,5 @@
+export const siteUrl = "https://dte.meilcli.net";
+export const siteTitle = "documented-track-event";
+export const siteDescription = "A example of documented track event";
+export const siteAuthor = "meil";
+export const siteStartYear = 2024;
