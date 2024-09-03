c62a1ca9ff38fcb0d908496ff8ac7c0b06be4235
MeilCli <ics.sora.meil@gmail.com>
2024-09-04 02:03:48 +0900

    fix constant

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,4 @@
-export const siteUrl = "https://documented-track-event.pages.dev/";
+export const siteUrl = "https://documented-track-event.pages.dev";
 export const siteTitle = "documented-track-event";
 export const siteDescription = "A example of documented track event";
 export const siteAuthor = "meil";
